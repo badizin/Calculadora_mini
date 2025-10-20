040225930e4ebdaae9e6d3a1ed5c5b03993cef93
Lucas Oliveira <luskoliveira@protonmail.com>
2025-10-20 12:16:22 -0300

    Update adicao.py

--- adicao.py
+++ adicao.py
@@ -1,4 +1,11 @@
 # Função adição
 def adicao(a, b):
-    "Retorna o valor da soma de dois números"
+    a = float(a)
+    b = float(b)
     return a + b
+
+
+if __name__ == "__main__":
+    a = input("Me fale um número: ")
+    b = input("Me fale um número: ")
+    print(f"O resultado da soma é {adicao(a, b)}")
